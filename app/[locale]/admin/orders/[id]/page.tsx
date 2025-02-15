876dde48ed9ab94e6515fae128f989bdb2dbf6ea
Sonu Rai <sonurai@Sonus-MacBook-Air.local>
2025-02-15 19:04:30 +0530

    bug fix

--- app/[locale]/admin/orders/[id]/page.tsx
+++ app/[locale]/admin/orders/[id]/page.tsx
@@ -10,9 +10,9 @@ export const metadata = {
   title: 'Admin Order Details'
 }
 
-const AdminOrderDetailsPage = async ({ params }: { params: { id: string } }) => {
-  const awaitedParams = await params; // Ensuring params is awaited
-  const { id } = awaitedParams;
+const AdminOrderDetailsPage = async (props: { params: { id: string } }) => {
+  const { params } = props;
+  const { id } = await params;
 
   const order = await getOrderById(id);
   if (!order) notFound();
