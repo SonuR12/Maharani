0c8d78c0f7d5b38553cfb344a1628d7e80b12c30
Sonu Rai <sonurai@Sonus-MacBook-Air.local>
2025-02-15 14:30:04 +0530

    bug Fix

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,41 +1,25 @@
-import mongoose, { Connection, ConnectOptions } from 'mongoose';
-
-// Define the type for the cached object
-interface MongooseCache {
-  conn: Connection | null;
-  promise: Promise<Connection> | null;
-}
+import mongoose from 'mongoose'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const globalAny: any = global;
-globalAny.mongoose = globalAny.mongoose || { conn: null, promise: null };
-const cached: MongooseCache = globalAny.mongoose;
+const cached = (global as any).mongoose || { conn: null, promise: null }
 
 export const connectToDatabase = async (
   MONGODB_URI = process.env.MONGODB_URI
 ) => {
-  if (cached.conn) return cached.conn;
+  if (cached.conn) return cached.conn
 
   if (!MONGODB_URI) throw new Error('❌ MONGODB_URI is missing');
 
   if (!cached.promise) {
-    // Define mongoose options with the correct type
-    const mongooseOptions: ConnectOptions = {
-      bufferCommands: false,
-      maxPoolSize: 5,
-      connectTimeoutMS: 10000,
-      socketTimeoutMS: 45000,
-    };
-
-    cached.promise = mongoose.connect(MONGODB_URI, mongooseOptions).then((mongoose) => {
+    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => {
       console.log('✅ MongoDB Connected Successfully!');
-      return mongoose.connection; // Return the connection object
+      return mongoose
     }).catch((err) => {
       console.error('❌ MongoDB Connection Error:', err);
       throw err;
     });
   }
 
-  cached.conn = await cached.promise;
-  return cached.conn;
-};
+  cached.conn = await cached.promise
+  return cached.conn
+}
